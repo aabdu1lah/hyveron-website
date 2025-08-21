2797de8eb33e142360f3428ec082008acc15aef5
Syed Abdullah <syedabdullah1722@gmail.com>
2025-08-21 19:41:18 +0500

    defined theme colors to be used in the project

---
+++ src/constants/colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+    whiteTone: 'rgb(248, 249, 250)',    // Light for text/accents
+    verdigris: 'rgb(71, 173, 163)',     // Accent color
+    blackTone: 'rgb(33, 37, 41)',       // Dark for background
+    darkGrey: 'rgb(50, 54, 58)',        // Slightly lighter dark for elements
+}
