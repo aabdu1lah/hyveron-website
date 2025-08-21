9f6ce626bfa66e5841351827ad06575804adfb43
Syed Abdullah <syedabdullah1722@gmail.com>
2025-08-21 19:56:04 +0500

    load assets into variables and export for usage

---
+++ src/assets/assets.js
@@ -0,0 +1,33 @@
+import hyveron_logo_black from './hyveron_Black - Logo.png'
+import hyveron_logo_dark from './hyveron_Dark - Logo.jpg'
+import hyveron_logo_white from './hyveron_White - Logo.png'
+import hyveron_logo_gradient from './hyveron_Gradient - Logo.jpg'
+import hyveron_logo_light from './hyveron_Light - Logo.jpg'
+import hyveron_mark_black from './hyveron_Black - Mark.png'
+import hyveron_mark_dark from './hyveron_Dark - Mark.jpg'
+import hyveron_mark_white from './hyveron_White - Mark.png'
+import hyveron_mark_gradient from './hyveron_Gradient - Mark.jpg'
+import hyveron_mark_light from './hyveron_Light - Mark.jpg'
+import hyveron_slogan_black from './hyveron_Black - Slogan.png'
+import hyveron_slogan_dark from './hyveron_Dark - Slogan.jpg'
+import hyveron_slogan_white from './hyveron_White - Slogan.png'
+import hyveron_slogan_gradient from './hyveron_Gradient - Slogan.jpg'
+import hyveron_slogan_light from './hyveron_Light - Slogan.jpg'
+
+export const assets = {
+    hyveron_logo_black,
+    hyveron_logo_dark,
+    hyveron_logo_white,
+    hyveron_logo_gradient,
+    hyveron_logo_light,
+    hyveron_mark_black,
+    hyveron_mark_dark,
+    hyveron_mark_white,
+    hyveron_mark_gradient,
+    hyveron_mark_light,
+    hyveron_slogan_black,
+    hyveron_slogan_gradient,
+    hyveron_slogan_light,
+    hyveron_slogan_dark,
+    hyveron_slogan_white,
+}
