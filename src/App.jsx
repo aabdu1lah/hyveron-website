b298f4f4e46490e3fda26925dd8596a1cd5c5b60
Syed Abdullah <syedabdullah1722@gmail.com>
2025-08-21 20:13:40 +0500

    added code to return prospective pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { colors } from "./constants/colors.js"
 
-const App = () => {
-    return (
-        <div>
+function App() {
+    const [activeSection, setActiveSection] = useState('home');
 
-        </div>
-    )
+    // Helper function to render sections based on active section state
+    const renderSection = () => {
+        switch (activeSection) {
+            case 'home':
+                return ()
+            case 'about':
+                return ()
+            case 'products':
+                return ()
+            case 'blog':
+                return ()
+            case 'contact':
+                return ()
+            default:
+                return ()
+        }
+    }
 }
 
 export default App
